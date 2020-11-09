b29e04d842975446fee3cd8a563dde7b03c42c00
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2020-11-09 18:03:55 +0900

    push

---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosError } from 'axios';
+
+export const extractErrorMsg = (error: AxiosError) => {
+  if (!error.response) {
+    return '서버에 접속할 수 없습니다';
+  } else {
+    return error.response.data.error.message || '에러 발생';
+  }
+};
+
+class AxiosInstanceCreator {
+  #instans: AxiosInstances;
+
+  constructor(config?: AxiosRequestConfig) {
+    this.#instans = axios.create(config);
+    console.log(this.#instans);
+  }
+
+  create() {
+    return this.#instance;
+  }
+}
+
+export default AxiosInstanceCreator;
